614c052bfa71a9c003491f274296b84fed947fd8
xTiiX <arthur.nordmann@supinfo.com>
2019-10-15 15:51:16 +0100

    Suppresion des anciens fichiers

--- app.py
+++ app.py
@@ -2,3 +2,37 @@
 import pygame as pg
 import random as rd
 
+###INIT###
+pg.init()
+
+
+###Vars###
+table = [[[],[],[]],
+         [[],[],[]],
+         [[],[],[]]]
+
+IsPlayer1 = True
+actual_sign = ""
+
+l =
+fenetre = pg.display.set_mode((l,L))
+
+
+
+###Defs / Fonctions###
+
+
+
+
+
+
+
+###Debut du programme###
+
+
+
+
+
+
+
+
