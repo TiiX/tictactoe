6719f9ce8095edc41bd911b9664a11a234c0399f
xTiiX <arthur.nordmann@supinfo.com>
2019-10-16 15:02:18 +0100

    Init + Boucle avec fermeture de la fenetre

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 ###Import###
 import pygame as pg
+from pygame.locals import *
 import random as rd
 
 ###Init###
@@ -19,7 +20,7 @@ actual_sign = ""
 l = 1920
 L = 1080
 
-window = pg.display.set_mode((l,L), "FULLSCREEN")
+window = pg.display.set_mode((l,L), pg.FULLSCREEN)
 
 points = [0,0]
 
@@ -30,12 +31,17 @@ points = [0,0]
 
 
 ###Start of the project###
-
 while _continue or points[0] != 3 or points[1] != 3:
 
-    pass
+    # Verification events
+    for event in pg.event.get():
+        if event.type == QUIT:
+            _continue = False
+            print("Hey !")
 
-
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                _continue = False
 
 #Quit the game
 pg.quit()
