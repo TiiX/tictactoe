c48f751d22d9a571029d9548f3397484453370b2
xTiiX <arthur.nordmann@supinfo.com>
2019-10-16 22:36:19 +0100

    /

--- app.py
+++ app.py
@@ -20,27 +20,38 @@ actual_sign = ""
 l = 1920
 L = 1080
 
-window = pg.display.set_mode((l,L), pg.FULLSCREEN)
+window = pg.display.set_mode((l,L))
 
 points = [0,0]
-
+winner = ""
 
 
 ###Definitions###
+def WinnerTest():
+    if points[0] == 3:
+        winner = "P1"
+        _continue = False
 
+    elif points[1] == 3:
+        winner = "P2"
+        _continue = False
 
+    return winner, _continue
 
 ###Start of the project###
-while _continue or points[0] != 3 or points[1] != 3:
+while _continue:
 
-    # Verification events
+    #Winner verification
+    WinnerTest()
+
+    # Events gestion
     for event in pg.event.get():
         if event.type == QUIT:
             _continue = False
-            print("Hey !")
 
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
+        elif event.type == KEYDOWN:
+            #Escape
+            if event.key in [K_ESCAPE]:
                 _continue = False
 
 #Quit the game
